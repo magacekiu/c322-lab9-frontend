9a0186768aa8c3976c720174e4e96931512609d7
magacek <70607808+magacek@users.noreply.github.com>
2024-04-05 12:16:59 -0400

    finished

--- configuration.js
+++ configuration.js
@@ -1,6 +1,6 @@
 const mode = 0;
 
-const host_local = "http://localhost:8080";
+const host_local = "https://ducks-service-latest-1-shia.onrender.com";
 
 const host_remote = "https://ducks-service-latest-1-shia.onrender.com";
 
